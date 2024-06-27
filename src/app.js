2ed8b382bb1508eaf61f6d76ded10270268fbaf9
prootv890 <imakeitreal@icloud.com>
2024-06-27 08:16:53 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,156 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import fragmentShader from "./shaders/fragment.glsl";
+import vertexShader from "./shaders/vertex.glsl";
+/**
+ * Canvas
+ **/
+const canvas = document.querySelector("canvas#webgl");
+
+/**
+ * Scene
+ **/
+const scene = new THREE.Scene();
+
+/**
+ * Loaders
+ **/
+
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
+/**
+ * Sizes
+ **/
+
+/**
+ * Parent Object
+ **/
+
+const parentObject = new THREE.Object3D();
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(sizes.pixelRatio);
+});
+
+/**
+ * Camera
+ **/
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+camera.position.set(2, 2, 5);
+scene.add(camera);
+
+/**
+ * Controls
+ **/
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ **/
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true,
+});
+renderer.setClearColor("#181818");
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
+
+/**
+ * Mobile Model
+ **/
+
+// Material
+const material = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+});
+
+let phone = null;
+gltfLoader.load("/phone/phone.glb", (gltf) => {
+  gltf.scene.scale.set(0.3, 0.3, 0.3);
+
+  phone = gltf.scene;
+  phone.rotation.y = Math.PI;
+  //   phone.position.set(0.5, -0.47, 1);
+
+  phone.traverse((child) => {
+    // Change the material
+    if (child.isMesh) child.material = material;
+  });
+  scene.add(phone);
+});
+
+/**
+ * Clock (optional)
+ **/
+const clock = new THREE.Clock();
+
+/**
+ * Lights
+ **/
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+directionalLight.castShadow = true;
+const pointLight = new THREE.PointLight(0xffffff, 5.5);
+pointLight.castShadow = true;
+directionalLight.position.set(0, 10, 0);
+scene.add(ambientLight, directionalLight, pointLight);
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.1);
+pointLight.position.y = 2;
+
+/**
+ * Helpers
+ * - Grid and Axes
+ **/
+scene.add(new THREE.AxesHelper(5), new THREE.GridHelper(5), pointLightHelper);
+
+/**
+ * Tick
+ **/
+
+const tick = () => {
+  // Update controls
+  controls.update();
+  const elapsedTime = clock.getElapsedTime();
+
+  // Rotate objects
+  if (phone !== null) {
+    phone.rotation.z = -elapsedTime * 0.1;
+    phone.rotation.y = elapsedTime * 0.2;
+  }
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+// Start the tick
+tick();
---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,8 @@
+void main()
+{
+
+    gl_FragColor =  vec4(1.0, 1.0, 0.0, 1.0);
+     // Check in 3js if there is something corresponding to this and replace with code
+    #include <tonemapping_fragment> // Tonemapping
+    #include <colorspace_fragment>  // Color space conversion
+}
